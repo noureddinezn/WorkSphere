4710a129fe9446f71c7d791322ef132e0abfa70e
noureddinezn <noureddinezouana@gmail.com>
2025-11-24 15:42:09 +0100

    la creation de addeventlistener efacer hidden est cree hidden

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ const popup = document.getElementById("popup");
 const form = document.getElementById("employeeForm");
 const unassignedList = document.getElementById("unassigned_list");
 const popupList = document.getElementById("popup_employees");
+
+openForm.addEventListener("click", () => {
+  popup.classList.remove("hidden");
+});
+
+closeForm.addEventListener("click", () => {
+  popup.classList.add("hidden");
+  document.getElementById("experienceList").innerHTML = "";
+  experiences = [];
+});
+
