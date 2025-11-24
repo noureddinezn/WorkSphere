c934fe711668f9017f426176fd74c8eb99e538bc
noureddinezn <noureddinezouana@gmail.com>
2025-11-24 15:44:11 +0100

    Ajout/Suppression dynamique des expériences dans le formulaire

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const form = document.getElementById("employeeForm");
 const unassignedList = document.getElementById("unassigned_list");
 const popupList = document.getElementById("popup_employees");
 
+
 openForm.addEventListener("click", () => {
   popup.classList.remove("hidden");
 });
@@ -16,4 +17,25 @@ closeForm.addEventListener("click", () => {
   document.getElementById("experienceList").innerHTML = "";
   experiences = [];
 });
+
+
+
+document.getElementById("addExperienceBtn").addEventListener("click", () => {
+  const div = document.createElement("div");
+  div.className = "popup_item";
+
+  div.innerHTML = `
+    <input type="text" placeholder="Titre" class="exp_title" required />
+    <input type="date" class="exp_start" required />
+    <input type="date" class="exp_end" required />
+    <button class="remove_btn removeExp">X</button>
+  `;
+  div.querySelector(".removeExp").addEventListener("click", () => {
+    div.remove();
+  });
+
+  document.getElementById("experienceList").appendChild(div);
+});
+
+
 
